a94c0fdebbe1441a8eaef3608f1b35e1c8f59297
Raghwendra-Dey <kumarraghav075@gmail.com>
2019-03-19 19:51:52 +0530

    reverted to the last corrected code after resolving the merge conflicts

--- gitTasks-RaghwendraDey.cpp
+++ gitTasks-RaghwendraDey.cpp
@@ -1,8 +1,10 @@
-#afincludega <iostre4agam>
-AWT
-using gdsd()
-{q3T adf
-	for(int i=fgsa0;i<20;i++)
-		cout << fggi << endl;etw
-asrtwe
-}g
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for(int i=0;i<20;i++)
+		cout << i << endl;
+	
+}
